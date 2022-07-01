4c0c2b48cbb0a0dccc02a883d9fdb55617da9162
nodecodex <jagjit22994@gmail.com>
2022-07-02 00:23:07 +0530

    mtportfolio edit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import { BrowserRouter as Router,  Route, BrowserRouter } from "react-router-dom";
+import React from 'react';
 import Home from "./components/Home";
 import About from "./components/About";
-import Services from "./components/Services";
 import ContactUs from "./components/ContactUs";
 import NavBar from './components/NavBar';
 import SocialLinks from './components/SocialLinks';
